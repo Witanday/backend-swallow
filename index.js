5f3378e602cdc672f99adeb3cca9db45541fdfb8
Witanday <witanday.cd@gmail.com>
2019-07-15 20:26:23 -0400

    Added welcome route

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use("/api/users/:id/messages",
 loginRequired, 
 ensureCorrectUser,
 messageRoutes);
-
+app.get("/", (req,res)=>{return res.json("welcome to Swallow")})
 app.get("/api/messages", loginRequired, async function(req,res,next){
     try{
 let messages = await db.Message.find()
--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const {signup, signin}= require("../handlers/auth")
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.get("/", (req,res)=>{return res.json({Message:"Server is Up!!!"})});
+
 
 
 module.exports= router;
